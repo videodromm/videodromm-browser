8708bb3bdfeb9e780bbbe63fd026d91ec4752ac7
Bruce LANE <bruce@batchass.fr>
2016-11-03 05:37:42 +0100

    st renamed to uv visual studio code launch.json

--- src/www/js/GlslEditor.js
+++ src/www/js/GlslEditor.js
@@ -35,14 +35,14 @@ uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 
 void main() {
-    vec2 st = gl_FragCoord.xy/iResolution.xy;
-    st.x *= iResolution.x/iResolution.y;
-	vec4 tex0 = texture2D(iChannel0,st);
-    vec4 tex1 = texture2D(iChannel1,st);
+    vec2 uv = gl_FragCoord.xy/iResolution.xy;
+    uv.x *= iResolution.x/iResolution.y;
+	vec4 tex0 = texture2D(iChannel0,uv);
+    vec4 tex1 = texture2D(iChannel1,uv);
 
-    st += vec2(.0);
+    uv += vec2(.0);
     vec3 color = vec3(1.);
-    color = vec3(st.x,st.y,tex0.x);
+    color = vec3(uv.x,uv.y,tex0.x);
 
     gl_FragColor = vec4(color,1.0);
 }`;
