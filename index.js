d2e0e60c64c4e36a5b90faac58611503a6b63bf6
Bruce LANE <bruce@batchass.fr>
2016-11-07 21:14:31 +0100

    websocket server is now moved to videodromm-server

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
 var express = require('express'),
-    app = express(),
-    nodemon = require('nodemon'),
-    winston = require('winston'),
-    http = require('http').Server(app),
+	app = express(),
+	nodemon = require('nodemon'),
+	winston = require('winston'),
+	http = require('http').Server(app);/*,
     uuid = require('uuid'),
     WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port:8088});
+    wss = new WebSocketServer({port:8088});*/
 
 app.use(express.static('src/www'));
-
+/*
 wss.on('connection', function(socket){
   socket.id = uuid.v4();
   console.log('a user connected');
@@ -28,7 +28,7 @@ wss.broadcast = function(msg, sender) {
     wss.clients.forEach(function(client){
         if (client.id !== sender) client.send(msg) & winston.info('Message sent!');
     });
-}
+}*/
 
 http.listen(process.env.port || 8080, function(){
     winston.info('Application server running!');
